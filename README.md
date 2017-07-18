95a3fd28a6b61f925879d3242c8dc65482124303
Zoltan Toth <ztoth@indigo.ca>
2017-07-18 12:19:13 -0400

    npm code

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ Responsive clamping component with _&laquo;Read more&raquo;_/_&laquo;Read less&r
 
 [**DEMO**](https://zoltantothcom.github.io/react-clamp-lines/)
 
+![react-clamp-lines](react-clamp.png "react-clamp-lines")
+
 ## install
 `npm install --save react-clamp-lines`
 
 ## features
- - built with and for `React`
+ - built for `React` with no dependencies
  - option for _&laquo;Read more&raquo;_ and  _&laquo;Read less&raquo;_ buttons
  - customizable ellipsis text
  - customizable CSS classes
@@ -19,20 +21,18 @@ Responsive clamping component with _&laquo;Read more&raquo;_/_&laquo;Read less&r
 ```js
 import React, { Component } from 'react';
 import React-DOM from 'react-dom';
-import ClampLines from '../ClampLines';
+import ClampLines from 'react-clamp-lines';
 
 class App extends Component {
   render() {
     return (
-      <div>
         <ClampLines
-          text={text_to_clamp}
-          lines="4"
-          ellipsis="..."
-          moreText="Expand"
-          lessText="Collapse"
-          className="custom-class" />
-      </div>
+            text={text_to_clamp}
+            lines="4"
+            ellipsis="..."
+            moreText="Expand"
+            lessText="Collapse"
+            className="custom-class" />
     );
   }
 }
@@ -40,7 +40,17 @@ class App extends Component {
 ReactDOM.render(<App />, document.getElementById('clamp'));
 ```
 
+The component and the or _&laquo;Read more&raquo;_ button always  have the `clamp-lines` and `clamp-lines__button` CSS classes respectively. In the example above the `custom-class` will be added to `clamp-lines`, so the output will be:
+
+```html
+<div class="clamp-lines custom-class">
+    <div>clamped text here...</div>
+    <button class="clamp-lines__button">Expand</button>
+</div>
+```
+
 ## API
+List of all available props with their default values and description.
 ```javascript
 <ClampLines
     text={String}
@@ -52,36 +62,17 @@ ReactDOM.render(<App />, document.getElementById('clamp'));
     className={String}
     debounce={Number} />
 ```
-#### text
-Text you wish to clamp.
 
-#### lines
-Number of visible lines.
-**Default: `3`**
-
-#### ellipsis
-Text content for the ellipsis - will appear after the clamped lines.
-**Default: `...`**
-
-#### buttons
-Should the  _&laquo;Read more&raquo;_ and  _&laquo;Read less&raquo;_ buttons appear.
-**Default: `true`**
-
-#### moreText
- _&laquo;Read more&raquo;_ button value.
-**Default: `Read more`**
-
-#### lessText
- _&laquo;Read less&raquo;_ button value.
-**Default: `Read less`**
-
-#### className
-CSS class names added to main component.
-
-#### debounce
-Milliseconds, the function is waiting before being triggered, after it stops being called.
-**Default: `300`**
-
+prop | type | default | description |
+-----|------|---------|-------------|
+text | {String} |  | Text you wish to clamp
+lines | {Number} | `3` | Number of visible lines
+ellipsis | {String} | `...` | Text content for the ellipsis - will appear after the clamped lines
+buttons | {Boolean} | `true` | The  _&laquo;Read more&raquo;_ and  _&laquo;Read less&raquo;_ buttons
+moreText | {String} | `Read more` |  _&laquo;Read more&raquo;_ button value
+lessText | {String} | `Read less` |  _&laquo;Read less&raquo;_ button value
+className | {String} |   | CSS class names added to component
+debounce | {Number} | `300` | Milliseconds, the function is waiting before being triggered, after it stops being called
 
 ## run the example
 ```bash
--- src/index.js
+++ src/index.js
@@ -1,52 +1,150 @@
 import React, { PureComponent } from 'react';
-import ReactDOM from 'react-dom';
-import ClampLines from './ClampLines';
-import './styles.css';
 
-let ipsum = {
-  text_1: 'Spicy jalapeno bacon ipsum dolor amet drumstick sirloin chuck shankle. Flank ribeye pancetta andouille ham hock. Turkey cow tenderloin landjaeger filet mignon hamburger. Pig tail strip steak pastrami t-bone venison bresaola biltong corned beef drumstick pork hamburger tri-tip. Tongue ham hock corned beef tri-tip meatball t-bone fatback andouille sirloin chuck jowl biltong pastrami. Ham hock ground round landjaeger tail strip steak. Ham sirloin pork loin salami spare ribs. Jerky cow short ribs ground round. Hamburger porchetta shankle meatloaf shank.',
-  text_2: 'Spicy jalapeno bacon ipsum dolor amet drumstick sirloin chuck shankle. Flank ribeye pancetta andouille ham hock. Turkey cow tenderloin landjaeger filet mignon hamburger.',
-};
+export default class ClampLines extends PureComponent {
+  constructor(props) {
+    super(props);
 
-class App extends PureComponent {
-  render() {
+    this.element = null;
+    this.original = props.text;
+    this.watch = true;
+    this.lineHeight = 0;
+    this.start = 0;
+    this.middle = 0;
+    this.end = 0;
+    this.state = {
+      noClamp: false,
+      text: '.',
+    };
+
+    this.action = this.action.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+
+    this.debounced = this.debounce(this.action, props.delay);
+  }
+
+  componentDidMount() {
+    this.lineHeight = this.element.clientHeight + 1;
+    this.clampLines();
+
+    if (this.watch) {
+      window.addEventListener('resize', this.debounced);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.debounced);
+    this.action = null;
+    this.clickHandler = null;
+  }
+
+  debounce(func, wait, immediate) {
+    let timeout;
+
+    return () => {
+      let context = this, args = arguments;
+      let later = () => {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  action() {
+    if (this.watch) {
+      this.setState({ noClamp: false });
+      this.clampLines();
+    }
+  }
+
+  clampLines() {
+    this.setState({ text: '' });
+
+    let maxHeight = this.lineHeight * this.props.lines + 1;
+
+    this.start = 0;
+    this.middle = 0;
+    this.end = this.original.length;
+
+    while (this.start <= this.end) {
+      this.middle = Math.floor((this.start + this.end) / 2);
+      this.element.innerText = this.original.slice(0, this.middle);
+      if (this.middle === this.original.length) {
+        this.setState({
+          text: this.original,
+          noClamp: true,
+        });
+        return;
+      }
+
+      this.moveMarkers(maxHeight);
+    }
+
+    this.element.innerText = this.original.slice(0, this.middle - 5) + this.getEllipsis();
+    this.setState({ text: this.original.slice(0, this.middle - 5) + this.getEllipsis() });
+  }
+
+  moveMarkers(maxHeight) {
+    if (this.element.clientHeight <= maxHeight) {
+      this.start = this.middle + 1;
+    } else {
+      this.end = this.middle - 1;
+    }
+  }
+
+  getClassName() {
+    let className = this.props.className || '';
+
+    return `clamp-lines ${className}`;
+  }
+
+  getEllipsis() {
+    return this.watch && !this.state.noClamp ? this.props.ellipsis : '';
+  }
+
+  getButton() {
+    if (this.state.noClamp || !this.props.buttons) return;
+
+    let buttonText = this.watch ? this.props.moreText : this.props.lessText;
+
     return (
-      <div className="container">
-        <h3>1. Default</h3>
-        <pre><code>{`<ClampLines text={text_to_clamp} />`}</code></pre>
+      <button className="clamp-lines__button" onClick={this.clickHandler}>
+        {buttonText}
+      </button>
+    );
+  }
 
-        <ClampLines text={ipsum.text_1} />
+  clickHandler(e) {
+    e.preventDefault();
 
-        <h3>2. Custom button labels, custom ellipsis and custom CSS class</h3>
-        <pre><code>{`<ClampLines
-  text={text_to_clamp}
-  lines="4"
-  moreText="Expand"
-  lessText="Collapse"
-  className="custom-class"
-  ellipsis="_ _" />`}</code></pre>
+    this.watch = !this.watch;
+    this.watch ? this.clampLines() : this.setState({ text: this.original });
+  }
 
-        <ClampLines
-          text={ipsum.text_1}
-          lines="4"
-          moreText="Expand"
-          lessText="Collapse"
-          className="custom-class"
-          ellipsis="_ _" />
+  render() {
+    if (!this.props.text) {
+      return null;
+    }
 
-        <h3>3. No buttons and increased debounce delay</h3>
-        <pre><code>{`<ClampLines
-  text={text_to_clamp}
-  buttons={false}
-  delay="900" />`}</code></pre>
-
-        <ClampLines
-          text={ipsum.text_1}
-          buttons={false}
-          delay="900" />
+    return (
+      <div className={this.getClassName()}>
+        <div ref={e => { this.element = e; }}>
+            {this.state.text}
+        </div>
+        {this.getButton()}
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('clamp'));
+ClampLines.defaultProps = {
+  buttons: true,
+  lines: 3,
+  delay: 300,
+  ellipsis: '...',
+  moreText: 'Read more',
+  lessText: 'Read less'
+};
