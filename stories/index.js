44f569a44edc925b8229738f692de7aad9a37b46
Brad Adams <hi@breadadams.com>
2018-02-22 03:56:54 +0100

    Add storybook
    + Add storybook
    + Update package.json

---
+++ stories/index.js
@@ -0,0 +1,49 @@
+import React, {Fragment} from 'react'
+import {storiesOf} from '@storybook/react'
+import NanoClamp from '../src/index'
+
+const string =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, dignissimos totam nam cumque ipsum autem placeat. Doloribus nesciunt id saepe quasi, quod, quis voluptatibus rerum at perspiciatis impedit ab nostrum.'
+
+const NanoClampWrap = ({background, lines, width}) => (
+  <div
+    style={{
+      width,
+      maxWidth: '100%',
+      marginBottom: 25,
+      padding: 20,
+      fontFamily: 'Helvetica, Arial, sans-serif',
+      fontSize: 14,
+      lineHeight: '20px',
+      background,
+      color: 'white',
+      boxSizing: 'border-box'
+    }}
+  >
+    <NanoClamp text={string} lines={lines} />
+  </div>
+)
+
+const examples = [
+  {
+    background: '#0984e3',
+    lines: 1,
+    width: 500
+  },
+  {
+    background: '#6c5ce7',
+    lines: 2,
+    width: 370
+  },
+  {
+    background: '#e84393',
+    lines: 3,
+    width: 270
+  }
+]
+
+storiesOf('NanoClamp', module).add('default', () => (
+  <Fragment>
+    {examples.map(({...props}) => <NanoClampWrap {...props} />)}
+  </Fragment>
+))
