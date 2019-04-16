493c4089de76882793136672101359af0292f3b3
Zoltan Toth <contact@zoltantoth.com>
2019-04-16 10:13:57 -0400

    Fix TS definitions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,17 @@
 import * as React from 'react';
 
-declare namespace ClampLines {
-  export interface ClampLinesProps {
-    text: string;
-    lines?: number;
-    ellipsis?: string;
-    buttons?: boolean;
-    moreText?: string;
-    lessText?: string;
-    className?: string;
-    delay?: number;
-    stopPropagation?: boolean;
-  }
+export interface ClampLinesProps {
+  text: string
+  lines?: number
+  ellipsis?: string
+  buttons?: boolean
+  moreText?: string
+  lessText?: string
+  className?: string
+  delay?: number
+  stopPropagation?: boolean
 }
 
-declare class ClampLines extends React.Component<ClampLines.ClampLinesProps> { }
+declare class ClampLines extends React.Component {}
 
-export = ClampLines;
+export default ClampLines
