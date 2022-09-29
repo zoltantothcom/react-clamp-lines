dd6cd12b5374dc662004b8a14cb285387b5d9b55
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2022-09-29 12:17:16 +0330

    [FEAT]: add text type to component for rendering html strings with style

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,20 @@
 import * as React from 'react';
 
 export interface ClampLinesProps {
-  text: string
-  id: string
-  lines?: number
-  ellipsis?: string
-  buttons?: boolean
-  moreText?: string
-  lessText?: string
-  className?: string
-  delay?: number
-  stopPropagation?: boolean
-  innerElement?: string
+	text: string;
+	id: string;
+	type: 'html' | 'text';
+	lines?: number;
+	ellipsis?: string;
+	buttons?: boolean;
+	moreText?: string;
+	lessText?: string;
+	className?: string;
+	delay?: number;
+	stopPropagation?: boolean;
+	innerElement?: string;
 }
 
 declare class ClampLines extends React.Component<ClampLinesProps> {}
 
-export default ClampLines
+export default ClampLines;
